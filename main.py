93bde17d4862e146e3f40cdb586f82d84b1c04f8
qinst64 <16003088+qinst64@users.noreply.github.com>
2019-03-31 23:09:59 +0800

    Revert "test pull request"

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import time
 #edit on branch edit2
-#print(time.time())
-tic = time.time()
-print("hello world")
-tac = time.time()
-print("printing 'Hello World' used {} seconds".format(tac - tic))
+print(time.time())
