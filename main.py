fec6c1352de3b9981b9c92ac7e0e9e31eb8a790f
cdzhang <cdzhang01@foxmail.com>
2019-03-31 23:02:36 +0800

    test pull request
    test pull request

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import time
 #edit on branch edit2
-print(time.time())
+#print(time.time())
+tic = time.time()
+print("hello world")
+tac = time.time()
+print("printing 'Hello World' used {} seconds".format(tac - tic))
